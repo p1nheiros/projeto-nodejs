e99460c644d7569723e5fc172cd72ae29a558891
p1nheiros <pinheiros.dev@gmail.com>
2024-08-07 20:40:40 -0300

    Foi realizado a implementação onde a API que busca o cep informado retorna as informações para o sistema.

--- pastanode/server.js
+++ pastanode/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+
 const app = express();
 const port = 3000;
 
@@ -7,18 +8,21 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 });
 
-// app.get('/novarota', (req, res) => {
-//     res.send('Nova rota criada.')
-// });
+app.get('/novarota', (req, res) => {
+    res.send('Nova rota criada.')
+});
 
-// app.get('/consulta-cep/:cep', async (req, res) => {
-//     const cep = req.params.cep;
+app.get('/consulta-cep/:cep', async (req, res) => {
+    const cep = req.params.cep;
 
-//     try {
-//         const.response = await axios.get('http://viacep.com.br/')
-//         res.json(response)
-//     }
-// });
+    try {
+        const response = await axios.get(`http://viacep.com.br/ws/${cep}/json/`)
+        res.json(response.data)
+    } catch (error) {
+        console.error('Erro ao fazer requisição:', error);
+        res.status(500).send('Erro ao consultar o CEP.');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
